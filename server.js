cabb7e05ba92c20fbb42249468660e148dbfd72f
fhalbiero <fhalbiero@gmail.com>
2020-03-23 16:48:52 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+const methodOverride = require('method-override');
+
+const articleRouter = require('./routes/articles');
+
+mongoose.connect('mongodb://localhost/blog', 
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndexes: true 
+    });
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(methodOverride('_method'));
+
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+       createdAt: 'desc' 
+    });
+
+    res.render('articles/index', { articles });
+});
+
+app.use('/articles', articleRouter);
+
+app.listen(5000);
